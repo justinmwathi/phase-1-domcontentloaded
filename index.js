73b931afd09ce7243347dccbfde01d99f6f2a445
justin <justinmwathi32@gmail.com>
2024-03-29 17:23:56 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded',replace);
+
+
+function replace(e){
+    const solution=document.getElementById('text');
+    solution.textContent='This is really cool!';
+}
